31c30c866df845b19a62da520676e5665d0477ea
Niels Ouvrard <niels.ouvrard@epitech.eu>
2022-05-22 14:42:01 +0200

    Enfants ok

--- src/main.c
+++ src/main.c
@@ -16,10 +16,10 @@
 
 typedef struct tree_s {
     char *name;
-    struct tree_s *frere_soeur;
+    int nbr_enfants;
     struct tree_s *conjoint;
-    struct tree_s *parents;
-    struct tree_s *enfants;
+    // struct tree_s *parents;
+    struct tree_s **enfants;
 } tree_s;
 
 typedef struct list_s {
@@ -46,12 +46,54 @@ list_s *add_new_node (list_s *list, char *name)
     list_s *new = malloc(sizeof(list_s));
     new->perso = malloc(sizeof(tree_s));
     new->perso->name = name;
+    new->perso->nbr_enfants = 0;
     new->perso->conjoint = NULL;
     new->perso->enfants = NULL;
-    new->perso->frere_soeur = NULL;
-    new->perso->parents = NULL;
+    // new->perso->parents = NULL;
     new->next = list;
     return new;
+}
+
+void ajoute_un_enfant (tree_s *parent, tree_s *enfant)
+{
+    tree_s **enfants_perso1 = malloc(sizeof(tree_s *) * (parent->nbr_enfants + 1));
+    // on copie l'ancien tableau dans le nouveau
+    for (int j = 0; j < parent->nbr_enfants; j++)
+        enfants_perso1[j] = parent->enfants[j];
+    enfants_perso1[parent->nbr_enfants] = enfant;
+    // si on avait déjà créé un tableau d'enfant, on le free
+    if (parent->nbr_enfants > 0)
+        free(parent->enfants);
+    parent->enfants = enfants_perso1;
+    parent->nbr_enfants++;
+}
+
+void print_all_perso (list_s *list)
+{
+    // * on affiche le tout
+    while (list) {
+        my_printf("%sNAME : %s\n%s", MY_COLOR_BLUE, list->perso->name, MY_COLOR_RESET);
+        my_printf("conj    :\t%s\n", list->perso->conjoint ? list->perso->conjoint->name : NULL);
+        my_printf("enfants :\t");
+        for (int i = 0; i < list->perso->nbr_enfants; i++)
+            my_printf("%s\t", list->perso->enfants[i]->name);
+        my_printf("\n\n\n");
+        list = list->next;
+    }
+}
+
+void free_all_perso (list_s *list)
+{
+    // * on affiche le tout
+    while (list) {
+        list_s *tmp = list;
+        list = list->next;
+        free(tmp->perso->name);
+        if (tmp->perso->nbr_enfants > 0)
+            free(tmp->perso->enfants);
+        free(tmp->perso);
+        free(tmp);
+    }
 }
 
 int main (int ac, char **av)
@@ -73,10 +115,10 @@ int main (int ac, char **av)
         // Parse 2 = conj 2
         tree_s *perso1 = where_is_it(list_names, parse[0]);
         tree_s *perso2 = where_is_it(list_names, parse[1]);
-        if (perso1 && perso2) {
-            perso2->conjoint = perso1;
-            perso1->conjoint = perso2;
-        }
+        if (!perso1 || !perso2) // gestion d'erreure
+            return 84;
+        perso2->conjoint = perso1;
+        perso1->conjoint = perso2;
         free_my_arr(parse);
     }
 
@@ -87,30 +129,21 @@ int main (int ac, char **av)
         // parse 1 = Pere
         // Parse 2 = Mere
         // Parse 3 = Enfant
-
-
-        // * a coder ici
-        // tree_s *perso1 = where_is_it(list_names, parse[0]);
-        // tree_s *perso2 = where_is_it(list_names, parse[1]);
-        // if (perso1 && perso2) {
-            // my_printf("%s conj %s\n", parse[0], parse[1]);
-        //     perso2->conjoint = perso1;
-        //     perso1->conjoint = perso2;
-        // }
+        tree_s *perso1 = where_is_it(list_names, parse[0]);
+        tree_s *perso2 = where_is_it(list_names, parse[1]);
+        tree_s *enfant = where_is_it(list_names, parse[2]);
+        if (!perso1 || !perso2 || !enfant) // gestion d'erreure
+            return 84;
+        ajoute_un_enfant(perso1, enfant);
+        ajoute_un_enfant(perso2, enfant);
         free_my_arr(parse);
     }
-    // my_printf("\n");
 
-    // for (int i = 0; i < names; i++)
-    //     my_printf("perso %s\t conjoint = %s\n", list_names[i].name, list_names[i].conjoint);
+    // * afficher
+    print_all_perso(list_names);
 
-    // * on affiche le tout
-    list_s *expl = list_names;
-    while (expl) {
-        my_printf("-> %s conj %s\n", expl->perso->name, expl->perso->conjoint ? expl->perso->conjoint->name : NULL);
-        expl = expl->next;
-    }
-    free_my_arr(src);
-    free(list_names);
+    // * free
+    free_all_perso(list_names);
+    free(src);
     return 0;
 }
